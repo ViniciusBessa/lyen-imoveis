44a7cddf4aaef0dc89416fe34b01e08ceedbd4c2
Vinicius <vinicius.b.silva3@gmail.com>
2022-07-20 13:34:29 -0300

    feat: footer implementado, onde o usuário pode encontrar imóveis por tipo de anúncio ou cidade, e navegação para as páginas de contato e sobre

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LocationsService } from '../shared/services/locations.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent implements OnInit, OnDestroy {
+  cities: string[] = [
+    'São Paulo',
+    'Sorocaba',
+    'Maringá',
+    'Macapá',
+    'Rio de Janeiro',
+    'Salvador',
+  ];
 
-  ngOnInit(): void {}
+  private locationsCitiesSubs!: Subscription;
+
+  constructor(private locationsService: LocationsService) {}
+
+  ngOnInit(): void {
+    this.locationsCitiesSubs = this.locationsService.getCities().subscribe({
+      next: ({ cities }) => (this.cities = cities.slice(0, 6)),
+      error: () =>
+        (this.cities = [
+          'São Paulo',
+          'Sorocaba',
+          'Maringá',
+          'Macapá',
+          'Rio de Janeiro',
+          'Salvador',
+        ]),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.locationsCitiesSubs.unsubscribe();
+  }
 }
